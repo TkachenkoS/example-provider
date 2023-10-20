5f8516a0d3195e5a200111714cd147d5487842d6
TkachenkoS <serhii.t@miviewis.com>
2023-10-20 08:37:53 -0500

    1020

--- src/Repositories/Walk.cs
+++ src/Repositories/Walk.cs
@@ -3,13 +3,16 @@ namespace Walks
 {
     public class Walk
     {
-        public Walk(int id, string name, string status) {
+        public Walk(int id, string name, string status)
+        {
             this.Id = id;
             this.Name = name;
+            this.Name1 = name;
             this.Status = status;
         }
         public string Status { get; set; }
         public string Name { get; set; }
+        public string Name1 { get; set; }
         public int Id { get; set; }
     }
 }
